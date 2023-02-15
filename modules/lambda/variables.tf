3305963e7271703f197b0603255cdc656505dc12
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-15 21:40:36 +0530

    Update variables.tf

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,7 +1,14 @@
 variable "filename" {
   description = "Provide the File Name."
+  default = ""
   }
 
 variable "handler" {
   description = "Provide the Handler"
+  default = ""
+}
+
+variable "functionname" {
+  description = "Provide the Handler"
+  default = ""
 }
