f396404870c894bf8784eeb3098bce5a168131cb
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-15 21:46:54 +0530

    Create variables.tf

---
+++ infrastructure/variables.tf
@@ -0,0 +1,14 @@
+variable "filename" {
+  description = "Provide the File Name."
+  default = "welcome.py"
+  }
+
+variable "handler" {
+  description = "Provide the Handler"
+  default = "welcome"
+}
+
+variable "functionname" {
+  description = "Provide the Handler"
+  default = "testfunction"
+}
