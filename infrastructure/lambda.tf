cd4b9f1eed8b4f6a6dbf2b1ce09854934e7e4064
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-16 12:39:59 +0530

    Update lambda.tf

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -14,3 +14,24 @@ module "lambda_function_1" {
   
 }
 
+  ######## Second Lambda##########
+  
+  data "archive_file" "lambda_function_2" {
+  type        = "zip"
+  source_dir  = "${path.module}/newcode"
+  output_path = "${path.module}/lambda_function_2.zip"
+}
+
+module "lambda_function_1" {
+  source         = "../modules/lambda"
+  filename       = data.archive_file.lambda_function_2.output_path
+  function_name  = "lambda_function_2
+  handler        = "lambda_function_2.hello"
+  #source_code_hash = data.archive_file.lambda_function_2.output_base64sha256
+  #source_code_hash = filebase64sha256(var.filename)
+  
+}
+ 
+  
+    
+  
