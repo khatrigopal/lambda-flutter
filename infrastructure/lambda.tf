175a8e7890b8e0e1f5694b1686813c6301b4a2fa
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-16 12:55:36 +0530

    Update lambda.tf

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -7,8 +7,26 @@ data "archive_file" "lambda_function_1" {
 module "lambda_function_1" {
   source         = "../modules/lambda"
   filename       = data.archive_file.lambda_function_1.output_path
+  role_arn        =  arn:aws:iam::558940753150:role/Spacelift_Test_Lambda_Function_Role
   function_name  = "lambda_function_1"
   handler        = "lambda_function_1.hello"
+  #source_code_hash = data.archive_file.lambda_function_1.output_base64sha256
+  #source_code_hash = filebase64sha256(var.filename)
+  
+}
+
+data "archive_file" "lambda_function_2" {
+  type        = "zip"
+  source_dir  = "${path.module}/newcode"
+  output_path = "${path.module}/lambda_function_2.zip"
+}
+
+module "lambda_function_2" {
+  source         = "../modules/lambda"
+  filename       = data.archive_file.lambda_function_2.output_path
+  role_arn        =  arn:aws:iam::558940753150:role/Spacelift_Test_Lambda_Function_Role
+  function_name  = "lambda_function_2"
+  handler        = "lambda_function_2.hello"
   #source_code_hash = data.archive_file.lambda_function_1.output_base64sha256
   #source_code_hash = filebase64sha256(var.filename)
   
