6bfd48383b4fb76747820014c1a8e6dca7829716
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 19:52:03 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "this" {
    role                           =  var.role_arn
    handler                        =  var.handler
    runtime                        = "python3.8"
-   source_code_hash               = filebase64sha256(filename)
+   
    
           
 }
