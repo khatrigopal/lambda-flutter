37fdb113d70f6c9550f1aa9ba9189e59d6190f82
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 11:28:14 +0000

    Update lambda.tf

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -11,6 +11,7 @@ module "lambda_function_1" {
   function_name  = "lambda_function_1"
   handler        =  "lambda_function_1.lambda_handler"
   source_code_hash = data.archive_file.lambda_function_1.output_base64sha256
+  publish = true
   #source_code_hash = filebase64sha256(var.filename)
   
 }
