85cc004069e7a675eb2471946ad8983b827dc7b6
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-17 11:26:13 +0000

    Update lambda.tf

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -10,7 +10,7 @@ module "lambda_function_1" {
   role_arn        =  "arn:aws:iam::558940753150:role/lambda-full-acces"
   function_name  = "lambda_function_1"
   handler        =  "lambda_function_1.lambda_handler"
-  #source_code_hash = data.archive_file.lambda_function_1.output_base64sha256
+  source_code_hash = data.archive_file.lambda_function_1.output_base64sha256
   #source_code_hash = filebase64sha256(var.filename)
   
 }
